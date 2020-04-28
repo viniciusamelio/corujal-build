afbdfb66d8942bdbada6992f965bf8ee6a1c2a24
Vinicius Amélio <vinicius.amelio@zoho.com>
2020-04-28 20:08:29 -0300

    init

---
+++ precache-manifest.bacd526aa897c8891332455478919508.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbcef0e6689cdb8bcb68d597ffe2c24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d294386ed5c99d00ac1e",
+    "url": "/static/css/main.b7525e04.chunk.css"
+  },
+  {
+    "revision": "9a390e65e67447945234",
+    "url": "/static/js/2.5a99c938.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a99c938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d294386ed5c99d00ac1e",
+    "url": "/static/js/main.fc0b00ad.chunk.js"
+  },
+  {
+    "revision": "40888d59b0424bdfb3b9",
+    "url": "/static/js/runtime-main.d1243198.js"
+  },
+  {
+    "revision": "382f71c790102d4a5cc85617baecdfd7",
+    "url": "/static/media/logo.382f71c7.png"
+  },
+  {
+    "revision": "b21b15ced8bc35b12d24bb029d53e336",
+    "url": "/static/media/mobile-wallpaper.b21b15ce.png"
+  },
+  {
+    "revision": "109f0089e1e369d87c0439a31017b225",
+    "url": "/static/media/wallpaper.109f0089.svg"
+  }
+]);
